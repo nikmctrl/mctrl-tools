fdfa709a33e973054aac90fac26ba4c20e781ea7
Nikolai Schimke <schimkeNikolai@gmail.com>
2025-02-15 23:21:47 +0000

    Refactor flake to use mctrl-formatter

--- flake.nix
+++ flake.nix
@@ -1,15 +1,9 @@
 {
-  description = "A very basic flake";
+  description = "A collection of MissionCtrl's pre-configured tools";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    mctrl-formatter.url = "github:nikmctrl/mctrl-formatter";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, mctrl-formatter, ... }: mctrl-formatter;
 }

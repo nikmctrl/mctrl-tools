42dc8c671597cec43b21b2c3c5eb9814533e04c7
Nikolai Schimke <schimkeNikolai@gmail.com>
2025-02-16 02:59:21 +0000

    Add pre-commit hooks and update flake configuration

--- flake.nix
+++ flake.nix
@@ -2,8 +2,30 @@
   description = "A collection of MissionCtrl's pre-configured tools";
 
   inputs = {
-    mctrl-formatter.url = "github:nikmctrl/mctrl-formatter";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    mctrl-formatter.url = "github:nikmctrl/mctrl-formatter?ref=git-hooks";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = { self, mctrl-formatter, ... }: mctrl-formatter.outputs;
+  outputs = { self, nixpkgs, mctrl-formatter, pre-commit-hooks, ... }: let 
+    base = mctrl-formatter.outputs;
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in base // (forAllSystems (system: {
+    checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = self.mctrl-hooks;
+        };
+      };
+
+      devShells = {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        };
+      };
+    })
+  );
 }

f901220765e65d346bc2c7fa4291ff4c8ad9c117
Nikolai Schimke <schimkeNikolai@gmail.com>
2025-02-25 17:44:31 +0000

    Complete mctrl-shell module refactoring with evalModules

--- mctrl-shell/default.nix
+++ mctrl-shell/default.nix
@@ -1,36 +1,9 @@
 { pkgs, lib, ... }:
 
-{
-  options.out = {
-    pkgs = lib.mkOption { type = lib.types.listOf lib.types.package; };
-
-    aliases = lib.mkOption {
-      type = lib.types.listOf (
-        lib.types.submodule {
-          options = {
-            name = lib.mkOption { type = lib.types.string; };
-            value = lib.mkOption { type = lib.types.string; };
-          };
-          # TODO: Add default aliases
-          # config = {
-          # name = "alias";
-          # value = "alias";
-          # };
-
-        }
-      );
-      files = lib.mkOption { type = lib.types.attrsOf lib.types.string; };
-    };
-  };
-
-  config = {
-    # out.pkgs = [ ];
-    out.aliases = [
-      {
-        name = "alias";
-        value = "alias";
-      }
-    ];
-
-  };
+pkgs.lib.evalModules {
+  modules = [
+    ./shells.nix
+    ./tools
+    ./out.nix
+  ];
 }
